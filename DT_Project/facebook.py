4c7a4fa32e351ceb4761c68f6fb570773e3057bd
MrAech <96242246+MrAech@users.noreply.github.com>
2023-11-28 18:18:43 +0530

    Update facebook.py

--- DT_Project/facebook.py
+++ DT_Project/facebook.py
@@ -1,25 +1,38 @@
+'''
 import facebook
-import schedule
-import time
-import os
+import sys
 
-def post_to_facebook():
-    # Initialize the Graph API client
-    graph = facebook.GraphAPI(access_token='face_access_token', version='3.0')
+def post_to_facebook(face_access_token, post_date, image_path, caption):
 
-    # Upload photo or video
-    graph.put_photo(image=open('image', 'rb'), message='Caption')
+    print("Apoligies cannot upload to facebook at the moment")
+    
+    
+    try:
+        # Initialize the Graph API client
+        graph = facebook.GraphAPI(access_token=face_access_token, version='3.0')
 
-    # Delete photo or video after posting (optional)
-    os.remove('image')
+        # Upload photo or video
+        with open(image_path, 'rb') as image:
+            graph.put_photo(image=image, message=caption)
 
-# Set the date and time for the post
-post_date = 'get_time'
+        print("Posted to Facebook successfully!")
+    except Exception as e:
+        print(f"Facebook post failed: {e}")
+        
 
-# Schedule the post at the specified date and time
-schedule.every().day.at(post_date).do(post_to_facebook)
+# Check if the script is run from the command line
+if __name__ == "__main__":
+    # Check if enough arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: python facebook.py <access_token> <post_date> <image_path> <caption>")
+        sys.exit(1)
 
-# Keep running the script to check for scheduled tasks
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    face_access_token = sys.argv[1]
+    post_date = sys.argv[2]
+    image_path = sys.argv[3]
+    caption = sys.argv[4]
+    
+if __name__ == "__main__":
+    # Call the post_to_facebook function with provided arguments: face_access_token, post_date, image_path, caption
+    post_to_facebook()
+'''
