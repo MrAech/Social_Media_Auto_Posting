a75de6d1ec51fa18c40f0bbe5a6ebd88a78d2bd7
MrAech <96242246+MrAech@users.noreply.github.com>
2023-11-28 18:20:29 +0530

    Update stlye.py

--- DT_Project/stlye.py
+++ DT_Project/stlye.py
@@ -5,12 +5,12 @@ def set_styles():
     style = ttk.Style()
 
     # Set the style for the main window
-    style.configure('TFrame', background='#f0f0f0', padding=5)
-    style.configure('TButton', background='#4CAF50', foreground='#FFFFFF', padding=(10, 5))
+    style.configure('TFrame', background='#1CD024', padding=5)
+    style.configure('TButton', background='#4CAF50', foreground='#32AEE0', padding=(10, 5), font=('Helvetica', 10, 'bold'))
     style.configure('TLabel', background='#f0f0f0', font=('Helvetica', 12), padding=(0, 5))
-    style.configure('Header.TLabel', font=('Helvetica', 16, 'bold'), background = '#333', foreground = '#FFFFFF')
-
-
+    style.configure('Header.TLabel', font=('Helvetica', 16, 'bold'), background='#333', foreground='#FFFFFF', padding=(10, 5))
+    style.configure('ImageButton.TButton', background='#1CD024', foreground='#333', padding=(10, 5), font=('Helvetica', 10, 'bold'))
+    return style
 
 def create_header_frame(parent, title):
     header_frame = ttk.Frame(parent, style='TFrame')
@@ -20,3 +20,5 @@ def create_header_frame(parent, title):
     header_label.grid(row=0, column=0)
 
     return header_frame
+
+
